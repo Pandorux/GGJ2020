678f9323a8db54f61565f99a897623072fbd9a72
Jared <jared.l.j.carey@gmail.com>
2020-01-31 23:31:56 +1000

    Hyreichy and Basic Snapping Implemented

---
+++ GGJ2020_Game/Assets/Scripts/Child.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Child : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject parent;
+
+    public GameObject GetParent()
+    {
+        return parent;
+    }
+
+    public void Unparent()
+    {
+        transform.SetParent(null);
+    }
+
+    public void ChangeParent(Transform parentTransform) 
+    {
+        transform.SetParent(parentTransform);
+    }
+
+}
---
+++ GGJ2020_Game/Assets/Scripts/Pot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    [SerializeField]
+    private Piece[] pieces;
+    
+    public void AddPiece(Piece newPiece)
+    {
+        GameObject newPieceObj = newPiece.gameObject;
+
+        // Set Pot as Parent
+        newPieceObj.transform.SetParent(transform);
+        newPieceObj.GetComponent<Child>().ChangeParent(transform);
+
+        // Make sure piece is in origin
+        newPieceObj.transform.localPosition = Vector3.zero;
+        newPieceObj.transform.localRotation = Quaternion.identity;
+        
+    }
+
+    #region Testing
+
+    [Header("Testing Variables")]
+
+    public Piece testPiece00;
+    public Piece testPiece01;
+    public Piece testPiece02;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            AddPiece(testPiece00);
+            testPiece00.gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            AddPiece(testPiece01);
+            testPiece01.gameObject.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            AddPiece(testPiece02);
+            testPiece02.gameObject.SetActive(true);
+        }   
+    }
+
+    #endregion
+
+}
