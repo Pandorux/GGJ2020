ed7d1d1279ea5a523d278cd9e0ffefb311c7899b
Catherine <catherine_b95@hotmail.com>
2020-02-01 20:40:44 +1000

    updated scenes and script updates

--- GGJ2020_Game/Assets/Scripts/PotController.cs
+++ GGJ2020_Game/Assets/Scripts/PotController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PotController : MonoBehaviour
 {
     public float speed = 5;
-    public Text countText;
-
+    public bool canPickUp = false;
+   
     private Rigidbody rb;
-    private int count;
+    private int goldCount;
+
+    public PieceEnum pieceNumber; 
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +29,43 @@ public class PotController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other) // when you hit the things, they deactivate
+    //// reactivate the corresponding piece on the bowl
+    //public void AddPiece(Piece newPiece)
+    //{
+    //    GameObject newPieceObj = newPiece.gameObject;
+
+    //    // Set Pot as Parent
+    //    newPieceObj.GetComponent<Child>().ChangeParent(transform);
+
+    //    // Make sure piece is in origin
+    //    newPieceObj.transform.localPosition = Vector3.zero;
+    //    newPieceObj.transform.localRotation = Quaternion.identity;
+
+    //    //pieces.Add(newPiece);
+    //}
+
+    void OnTriggerEnter(Collider other) 
     {
-        if (other.gameObject.CompareTag("Gold"))
+        
+        if (other.gameObject.CompareTag("Gold")) // **GOLD** when you hit gold, they deactivate and add 1 to your count
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
+            goldCount = goldCount + 1;
+         
+
         }
+
+        // check if you can pick up (is goldCount = 3?)
+        if (goldCount == 3)
+        {
+            //other.gameObject.SetActive(false); //deactivate piece on the floor
+            canPickUp = true;
+
+        }
+
+        //if (other.gameObject.CompareTag("Piece") & canPickUp = true) //AND canPickUp = true         // **PIECES** if the piece you hit is a Piece, check if you can pick up, they deactivate and you reactivate a piece to bowl
+        //{
+        //    AddPiece(other.g)
+        //}
     }
 }
