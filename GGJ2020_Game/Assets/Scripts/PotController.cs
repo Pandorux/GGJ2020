8f0e549449e961d86ccebc17605876f1611a7929
Catherine <catherine_b95@hotmail.com>
2020-02-01 13:10:42 +1000

    pot controller ?

---
+++ GGJ2020_Game/Assets/Scripts/PotController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotController : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // talk to the rigid body
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // move n shit
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    //void OnTriggerEnter(Collider other) // when you hit the things, they deactivate
+    //{
+    //    if (other.gameObject.CompareTag("Pick Up"))
+    //    {
+    //        other.gameObject.SetActive(false);
+    //    }
+    //}
+}
